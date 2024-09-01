eb18e5fb812b413cfcd23ca69c79acf9092e5902
refcell <abigger87@gmail.com>
2024-09-01 08:45:58 -0400

    feat(primitives): arbitrary impls

--- crates/primitives/src/addresses.rs
+++ crates/primitives/src/addresses.rs
@@ -55,6 +55,34 @@ pub struct AddressList {
     pub challenger: Option<Address>,
 }
 
+#[cfg(any(test, feature = "arbitrary"))]
+impl arbitrary::Arbitrary<'_> for AddressList {
+    fn arbitrary(g: &mut arbitrary::Unstructured<'_>) -> arbitrary::Result<Self> {
+        Ok(Self {
+            address_manager: Address::from(g.arbitrary::<[u8; 20]>()?),
+            l1_cross_domain_messenger_proxy: Address::from(g.arbitrary::<[u8; 20]>()?),
+            l1_erc721_bridge_proxy: Address::from(g.arbitrary::<[u8; 20]>()?),
+            l1_standard_bridge_proxy: Address::from(g.arbitrary::<[u8; 20]>()?),
+            l2_output_oracle_proxy: Some(Address::from(g.arbitrary::<[u8; 20]>()?)),
+            optimism_mintable_erc20_factory_proxy: Address::from(g.arbitrary::<[u8; 20]>()?),
+            optimism_portal_proxy: Address::from(g.arbitrary::<[u8; 20]>()?),
+            system_config_proxy: Address::from(g.arbitrary::<[u8; 20]>()?),
+            system_config_owner: Address::from(g.arbitrary::<[u8; 20]>()?),
+            proxy_admin: Address::from(g.arbitrary::<[u8; 20]>()?),
+            proxy_admin_owner: Address::from(g.arbitrary::<[u8; 20]>()?),
+            guardian: Address::from(g.arbitrary::<[u8; 20]>()?),
+            anchor_state_registry_proxy: Some(Address::from(g.arbitrary::<[u8; 20]>()?)),
+            delayed_weth_proxy: Some(Address::from(g.arbitrary::<[u8; 20]>()?)),
+            dispute_game_factory_proxy: Some(Address::from(g.arbitrary::<[u8; 20]>()?)),
+            fault_dispute_game: Some(Address::from(g.arbitrary::<[u8; 20]>()?)),
+            mips: Some(Address::from(g.arbitrary::<[u8; 20]>()?)),
+            permissioned_dispute_game: Some(Address::from(g.arbitrary::<[u8; 20]>()?)),
+            preimage_oracle: Some(Address::from(g.arbitrary::<[u8; 20]>()?)),
+            challenger: Some(Address::from(g.arbitrary::<[u8; 20]>()?)),
+        })
+    }
+}
+
 impl AddressList {
     /// Sets zeroed addresses to [`Option::None`].
     pub fn zero_proof_addresses(&mut self) {
@@ -82,5 +110,36 @@ impl AddressList {
         if self.challenger == Some(Address::ZERO) {
             self.challenger = None;
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn zero_proof_addresses() {
+        let mut addresses = AddressList {
+            anchor_state_registry_proxy: Some(Address::ZERO),
+            delayed_weth_proxy: Some(Address::ZERO),
+            dispute_game_factory_proxy: Some(Address::ZERO),
+            fault_dispute_game: Some(Address::ZERO),
+            mips: Some(Address::ZERO),
+            permissioned_dispute_game: Some(Address::ZERO),
+            preimage_oracle: Some(Address::ZERO),
+            challenger: Some(Address::ZERO),
+            ..Default::default()
+        };
+
+        addresses.zero_proof_addresses();
+
+        assert_eq!(addresses.anchor_state_registry_proxy, None);
+        assert_eq!(addresses.delayed_weth_proxy, None);
+        assert_eq!(addresses.dispute_game_factory_proxy, None);
+        assert_eq!(addresses.fault_dispute_game, None);
+        assert_eq!(addresses.mips, None);
+        assert_eq!(addresses.permissioned_dispute_game, None);
+        assert_eq!(addresses.preimage_oracle, None);
+        assert_eq!(addresses.challenger, None);
     }
 }
