a7f742ffaf4414c7f86633759985cc4fd565a5bf
refcell <abigger87@gmail.com>
2024-11-10 18:07:26 -0500

    feat: net crate

---
+++ crates/net/README.md
@@ -0,0 +1,42 @@
+## Consensus Network Library
+
+Contains a gossipsub driver to run discv5 peer discovery and block gossip.
+
+### Example
+
+> **Warning**
+>
+> Notice, the socket address uses `0.0.0.0`.
+> If you are experiencing issues connecting to peers for discovery,
+> check to make sure you are not using the loopback address,
+> `127.0.0.1` aka "localhost", which can prevent outward facing connections.
+
+```rust,no_run
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use alloy_primitives::address;
+use superchain_net::driver::NetworkDriver;
+
+// Build the network driver.
+let signer = address!("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+let socket = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 9099);
+let driver = NetworkDriver::builder()
+    .with_chain_id(10) // op mainnet chain id
+    .with_unsafe_block_signer(signer)
+    .with_gossip_addr(socket)
+    .build()
+    .expect("Failed to builder network driver");
+
+// Call `.start()` on the driver.
+driver.start().expect("Failed to start network driver");
+
+println!("NetworkDriver started.");
+```
+
+[!WARNING]: ###example
+
+### Acknowledgements
+
+Largely based off [magi]'s [p2p module][p2p].
+
+[magi]: https://github.com/a16z/magi
+[p2p]: https://github.com/a16z/magi/tree/master/src/network
---
+++ crates/net/src/gossip/driver.rs
@@ -0,0 +1,90 @@
+//! Consensus-layer gossipsub driver for Optimism.
+
+use crate::gossip::{
+    behaviour::Behaviour,
+    event::Event,
+    handler::{BlockHandler, Handler},
+};
+use eyre::Result;
+use futures::stream::StreamExt;
+use libp2p::{swarm::SwarmEvent, Multiaddr, Swarm};
+use tracing::{debug, error, info};
+
+/// A [libp2p::Swarm] instance with an associated address to listen on.
+pub struct GossipDriver {
+    /// The [libp2p::Swarm] instance.
+    pub swarm: Swarm<Behaviour>,
+    /// The address to listen on.
+    pub addr: Multiaddr,
+    /// Block handler.
+    pub handler: BlockHandler,
+}
+
+impl GossipDriver {
+    /// Creates a new [GossipDriver] instance.
+    pub fn new(swarm: Swarm<Behaviour>, addr: Multiaddr, handler: BlockHandler) -> Self {
+        Self { swarm, addr, handler }
+    }
+
+    /// Listens on the address.
+    pub fn listen(&mut self) -> Result<()> {
+        self.swarm.listen_on(self.addr.clone()).map_err(|_| eyre::eyre!("swarm listen failed"))?;
+        info!("Swarm listening on: {:?}", self.addr);
+        Ok(())
+    }
+
+    /// Returns a mutable reference to the Swarm's behaviour.
+    pub fn behaviour_mut(&mut self) -> &mut Behaviour {
+        self.swarm.behaviour_mut()
+    }
+
+    /// Attempts to select the next event from the Swarm.
+    pub async fn select_next_some(&mut self) -> SwarmEvent<Event> {
+        self.swarm.select_next_some().await
+    }
+
+    /// Returns the number of connected peers.
+    pub fn connected_peers(&self) -> usize {
+        self.swarm.connected_peers().count()
+    }
+
+    /// Dials the given [`Option<Multiaddr>`].
+    pub async fn dial_opt(&mut self, peer: Option<impl Into<Multiaddr>>) {
+        let Some(addr) = peer else {
+            return;
+        };
+        match self.dial(addr).await {
+            Ok(_) => info!("Dialed peer"),
+            Err(e) => error!("Failed to dial peer: {:?}", e),
+        }
+    }
+
+    /// Dials the given [Multiaddr].
+    pub async fn dial(&mut self, peer: impl Into<Multiaddr>) -> Result<()> {
+        let addr: Multiaddr = peer.into();
+        self.swarm.dial(addr).map_err(|e| eyre::eyre!("dial failed: {:?}", e))?;
+        Ok(())
+    }
+
+    /// Handles the [`SwarmEvent<Event>`].
+    pub fn handle_event(&mut self, event: SwarmEvent<Event>) {
+        if let SwarmEvent::Behaviour(Event::Gossipsub(libp2p::gossipsub::Event::Message {
+            propagation_source: src,
+            message_id: id,
+            message,
+        })) = event
+        {
+            debug!("Received message with topic: {}", message.topic);
+            if self.handler.topics().contains(&message.topic) {
+                debug!("Handling message with topic: {}", message.topic);
+                let status = self.handler.handle(message);
+                debug!("Reporting message validation result: {:?}", status);
+                _ = self
+                    .swarm
+                    .behaviour_mut()
+                    .gossipsub
+                    .report_message_validation_result(&id, &src, status);
+            }
+        }
+    }
+}
