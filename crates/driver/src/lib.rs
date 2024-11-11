011e6f3fd9576572b38ec23a2e161a0986f16508
refcell <abigger87@gmail.com>
2024-11-11 07:32:07 -0500

    feat: driver

--- crates/driver/src/lib.rs
+++ crates/driver/src/lib.rs
@@ -3,8 +3,4 @@
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 
-pub mod builder;
-pub mod discovery;
-pub mod driver;
-pub mod gossip;
-pub mod types;
+pub mod pipeline;
